02223cea3840475b7e2d857d390186ca63f84a5d
Leighann.Gant <leighann.gant@FT-MW5124.local>
2018-08-20 16:03:46 +0100

    data validator class created - slack_token check added

--- lib/data_validator.py
+++ lib/data_validator.py
@@ -1,3 +1,10 @@
+import os 
+
 class DataValidator:
     def __init__(self,data):
         self.data = data
+    
+    def validToken(self):
+        if self.data['token'] != os.environ["SLACK_TOKEN"]:
+            raise Exception("Bad Request, Slack token invalid")
+        return True 
--- test/test_data_validator.py
+++ test/test_data_validator.py
@@ -1,9 +1,19 @@
 import unittest
-from lib.data_validator import DataValidator
+import mock 
+import os
 
+from lib.data_validator import DataValidator
+mock_data = { "token": "123345","pattern":"datatechnology.etldashboard", "systemCode":"citrixxendesktop", "email":"test.email@ft.com"}
 class EventDataExtractorTestSpec(unittest.TestCase):
     def setUp(self):
-        self.DataValidator = DataValidator()
+        self.dataValidator = DataValidator(mock_data)
 
-    def test_token_value_is_valid(self):
-        self.assertTrue(self.DataValidator())
+    @mock.patch.dict(os.environ,{'SLACK_TOKEN':'123345'})
+    def test_validToken_returns_true_when_valid(self):
+        self.assertTrue(self.dataValidator.validToken())
+    
+    @mock.patch.dict(os.environ,{'SLACK_TOKEN':'123345897'})
+    def test_validToken_raises_exception_when_invalid(self):
+        with self.assertRaises(Exception) as context:
+            self.dataValidator.validToken()
+        self.assertEqual('Bad Request, Slack token invalid',str(context.exception))
