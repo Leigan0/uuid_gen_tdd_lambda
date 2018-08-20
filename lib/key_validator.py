39490010491b25b320e66415e5e17e03ccaa21fd
Leighann.Gant <leighann.gant@FT-MW5124.local>
2018-08-20 13:21:15 +0100

    function added to return keys from event

--- lib/key_validator.py
+++ lib/key_validator.py
@@ -1,8 +1,32 @@
 from urlparse import parse_qs
+import re 
 
 class KeyValidator:
     def __init__(self,event):
         self.event = event
     
-    def get_body(self):
-        return self.event['body']
+    def get_keys(self):
+        return self.get_args()
+    
+    def get_args(self):
+        body = body = parse_qs(self.event['body'])
+        try:
+            text = body['text']
+            values = text[0].split()
+        except KeyError as error:
+            print('Body missing {} key'.format(error))
+            raise
+        
+        try:
+            keys = {
+                "token": body['token'][0],
+                "pattern": re.sub('\.$', '', values[0]),
+                "systemCode": values[1],
+                "email": values[2]
+                }
+        except IndexError as error:
+            print(error)
+            print("A mandatory field is missing")
+            raise
+
+        return keys
--- test/test_key_validator.py
+++ test/test_key_validator.py
@@ -13,7 +13,7 @@ mock_event = {
     'stageVariables': None,
     'requestContext': {
     },
-    'body': 'token=123345&team_id=testTeam&team_domain=financialtimes&channel_id=12**12**&channel_name=directmessage&user_id=testUser**&user_name=test_user&command=%2Fgraphiteuuid&text=datatechnology.etldashboard+etl-dashboard+data.platform%40ft.com&response_url=https%3A%2F%2Fhooks.slack.com',
+    'body': 'token=123345&team_id=testTeam&team_domain=financialtimes&channel_id=12**12**&channel_name=directmessage&user_id=testUser**&user_name=test_user&command=%2Fgraphiteuuid&text=datatechnology.etldashboard+citrixxendesktop+test.email%40ft.com&response_url=https%3A%2F%2Fhooks.slack.com',
     'isBase64Encoded': False
     }
 
@@ -22,5 +22,20 @@ class KeyValidatorTestSpec(unittest.TestCase):
     def setUp(self):
         self.keyValidator = KeyValidator(mock_event)
 
-    def test_it_extracts_body_from_request(self):
-        self.assertEqual(self.keyValidator.get_body(), 'token=123345&team_id=testTeam&team_domain=financialtimes&channel_id=12**12**&channel_name=directmessage&user_id=testUser**&user_name=test_user&command=%2Fgraphiteuuid&text=datatechnology.etldashboard+etl-dashboard+data.platform%40ft.com&response_url=https%3A%2F%2Fhooks.slack.com')
+    def test_it_extracts_token_patten_systemCode_email_from_request(self):
+        self.assertEqual(self.keyValidator.get_keys(), { "token": "123345","pattern":"datatechnology.etldashboard", "systemCode":"citrixxendesktop", "email":"test.email@ft.com"})
+
+    def test_it_raises_error_if_not_passed_test_key_within_body(self):
+        mock_event['body'] = 'token=123345'
+        keyValidator = KeyValidator(mock_event)
+        with self.assertRaises(KeyError) as context:
+            keyValidator.get_keys()
+        self.assertTrue('text' in context.exception)
+        
+    def test_it_raises_error_if_not_passed_test_three_arguments_within_text_key(self):
+        mock_event['body'] = 'token=123345&text=pattern+systemCode'
+        keyValidator = KeyValidator(mock_event)
+        with self.assertRaises(IndexError) as context:
+            keyValidator.get_keys()
+        self.assertTrue('list index out of range' in context.exception)
+
